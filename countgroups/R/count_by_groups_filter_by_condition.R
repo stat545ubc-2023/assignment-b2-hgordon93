f77f6d3f5cd78686644778dbb857ef089c693b07
hgordon93 <immramaecology@gmail.com>
2023-11-06 19:55:35 -0800

    Documentation updated

--- countgroups/R/count_by_groups_filter_by_condition.R
+++ countgroups/R/count_by_groups_filter_by_condition.R
@@ -1,3 +1,25 @@
+#' Count observations by groups and/or those which meet a condition
+#'
+#' This is a function that summarises the number of observations for one or more variables within a data frame. It can be used to compute the number of  observations of one or more grouped variables, the number of observations that meet a specified condition, or a combination of the two. It uses functions from the tidyverse.
+#'
+#' @param df The first argument is named 'df' as a shorthand for data frame. This is a required argument.
+#' @param groups This argument is named 'groups' to refer to the grouping variable(s) that will be input to the group_by function. It must be an object or use the form c(x,y) if multiple variables are used for grouping. It is not required.
+#' @param cond This argument is named 'cond' as a shorthand for condition, to refer to the condition that will be specified in the filter function. This argument must be a logical vector (eg sex=="female"). It is not required.
+#'
+#' @return An object of the same type as `.df` containing the grouped variables and the corresponding number of observations (n) for each row; if no grouping was  applied and only a filtering condition was used, the output will be a 1x1 object containing the number of observations (n) that meet the condition.
+#'
+#'  The output has the following properties:
+#' * Each group is summarised down to one row.
+#' * Columns are not modified; column "n" for observations is created.
+#' * Data frame attributes are preserved.
+#'
+#' @export
+#'
+#' @examples
+#' count_by_groups_filter_by_condition(df=vancouver_trees, c(neighbourhood_name, genus_name), cond= date_planted >"2000-01-01")
+#' count_by_groups_filter_by_condition(df=penguins, c(island, species))
+#' count_by_groups_filter_by_condition(df=penguins, cond=sex=="female")
+#'
 count_by_groups_filter_by_condition <- function(df, groups, cond) {
   df %>%
     group_by(pick({{ groups }})) %>%
